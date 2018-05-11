d7472d04adf5f30eb3a28f4917d20c2969dd687e
jerr <jerry@fam-base.net>
2018-05-10 21:17:38 -0400

    pretty

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,5 +1,5 @@
 
-set(CN_PROJECT_NAME "karbowanec")
-set(CN_CURRENCY_DISPLAY_NAME "Карбованець")
-set(CN_CURRENCY_TICKER "KRB")
+set(CN_PROJECT_NAME "pennykoin")
+set(CN_CURRENCY_DISPLAY_NAME "Pennykoin")
+set(CN_CURRENCY_TICKER "PK")
 set(CN_VERSION 1.1.4)
