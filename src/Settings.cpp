e97614326eff0c952098b0187bf02f413c5ac370
jerr <jerry@fam-base.net>
2018-05-14 07:17:29 -0400

    added pool fixed logfiles

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -55,8 +55,8 @@ void Settings::load() {
   }
 
   QStringList defaultPoolList;
-  //defaultPoolList << "xdn.miner.center:4555" << "duckpool.mooo.com:2222" << "xdn.poolto.be:11010";
-  defaultPoolList << " ";
+  
+  defaultPoolList << "pk.youpool.io:5553";
   if (!m_settings.contains(OPTION_MINING_POOLS)) {
     setMiningPoolList(QStringList() << defaultPoolList);
   } else {
